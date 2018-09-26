61271a280b817a45b5eafe8cf1b09b6398a66a32
Amit Vishwakarma <amit.excellence@gmail.com>
2018-09-26 17:36:41 +0530

    #3 Method to get BeautifulSoup object for given URL

--- src/bs/soap.py
+++ src/bs/soap.py
@@ -1,4 +1,20 @@
+from bs4 import BeautifulSoup
+from urllib.request import Request, urlopen
+
+hdr = {'User-Agent': 'Mozilla/5.0'}
+
+
 class Soap:
 
     def __init__(self):
         pass
+
+    @staticmethod
+    def get_soap(url):
+        try:
+            req = Request(url, headers=hdr)
+            page = urlopen(req)
+            soap = BeautifulSoup(page, "lxml")
+            return soap
+        except Exception as e:
+            print('Soap error for: '+url+": ", e)
