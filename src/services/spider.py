3ff05655bf22d5ccaa9bcad1dc2815fcc7ac97ed
Amit Vishwakarma <amit.excellence@gmail.com>
2018-09-27 12:42:09 +0530

    #4 Method to get practice and contest problem codes for username

--- src/services/spider.py
+++ src/services/spider.py
@@ -1,4 +1,30 @@
+from collections import OrderedDict
+
+from src.bs.soap import Soap
+from src.utils.util import Util
+
+BASE_URL = "https://www.codechef.com"
+PROFILE_URL = BASE_URL + "/users/"
+
+
 class Spider:
 
     def __init__(self):
         pass
+
+    @staticmethod
+    def get_problems_codes(username):
+        soup = Soap.get_soap(PROFILE_URL + username)
+        if soup is None:
+            return None
+        articles = soup.findAll("div", {"class": "content"})[3].findAll("article")
+        if len(articles) > 0:
+            problems = OrderedDict()
+            ps = articles[0].findAll("p")
+            problems["practice"] = list(map(Util.practice_link_splitter, ps[0].findAll("a")))
+            for p in ps[1:]:
+                contest_name = p.findAll("strong")[0].text.replace(":", "")
+                problems[contest_name] = [x.text for x in p.findAll("a")]
+            return problems
+        else:
+            print("No problem codes found")
--- src/utils/util.py
+++ src/utils/util.py
@@ -2,3 +2,7 @@ class Util:
 
     def __init__(self):
         pass
+
+    @staticmethod
+    def practice_link_splitter(link):
+        return link.get("href").split(",")[0].split("/")[2]
