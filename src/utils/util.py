3cde34d7a08648d52939062f6c63dc8522c85082
Amit Vishwakarma <amit.excellence@gmail.com>
2018-09-27 13:52:54 +0530

    #5 Util methods to get file extension for language and solution status

--- src/utils/util.py
+++ src/utils/util.py
@@ -6,3 +6,33 @@ class Util:
     @staticmethod
     def practice_link_splitter(link):
         return link.get("href").split(",")[0].split("/")[2]
+
+    @staticmethod
+    def get_solution_status(img_src):
+        if "cross-icon" in img_src:
+            return "WA"
+        if "tick-icon" in img_src:
+            return "AC"
+        if "runtime-error" in img_src:
+            return "RE"
+        if "alert-icon" in img_src:
+            return "CE"
+        if "clock_error" in img_src:
+            return "TLE"
+        return None
+
+    @staticmethod
+    def get_file_extension_for_language(language):
+        if "C++" in language:
+            return "cpp"
+        if "PY" in language:
+            return "py"
+        if "JAVA" in language:
+            return "java"
+        if "C" in language:
+            return "c"
+        if "PAS" in language:
+            return "pas"
+        if "JAR" in language:
+            return "jar"
+        return "txt"
