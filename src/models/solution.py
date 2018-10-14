25c7e0da465bad53c8678c1165dcaa496943ed3d
Amit Vishwakarma <amit.excellence@gmail.com>
2018-10-14 13:22:09 +0530

    #6 Class for variables Solution model

--- src/models/solution.py
+++ src/models/solution.py
@@ -1,7 +1,13 @@
 class Solution:
 
-    def __init__(self):
-        pass
+    def __init__(self, contest, problem, s_id, status, extension, partial, text):
+        self.contest = contest
+        self.problem = problem
+        self.sId = s_id
+        self.status = status
+        self.extension = extension
+        self.partial = partial
+        self.text = text
 
     def __repr__(self):
-        pass
+        return self.contest+" "+self.problem+" "+self.sId
